98b80e23822b8483c799d46dcf9ce256cef4fbab
LIM <LIM@DESKTOP-1NABISU>
2019-03-21 08:02:51 +0200

    Scanner int number=scanner.nextInt();

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,30 +1,45 @@
+import java.util.Scanner;
 
 public class Gugudan {
 
 	public static void main(String[] args) {
 		//2단
-		System.out.println("2단"); 
-		System.out.println(2*1); 
-		System.out.println(2*2);
-		System.out.println(2*3);
-		System.out.println(2*4);
-		System.out.println(2*5);
-		System.out.println(2*6);
-		System.out.println(2*7);
-		System.out.println(2*8);
-		System.out.println(2*9);
-		
-		//3단
-		System.out.println("3단");
-		System.out.println(3*1);
-		System.out.println(3*2);
-		System.out.println(3*3);
-		System.out.println(3*4);
-		System.out.println(3*5);
-		System.out.println(3*6);
-		System.out.println(3*7);
-		System.out.println(3*8);
-		System.out.println(3*9);
+ 
+//		System.out.println("2단"); 
+//		System.out.println(2*1); 
+//		System.out.println(2*2);
+//		System.out.println(2*3);
+//		System.out.println(2*4);
+//		System.out.println(2*5);
+//		System.out.println(2*6);
+//		System.out.println(2*7);
+//		System.out.println(2*8);
+//		System.out.println(2*9);
+//		//
+//		//3단
+//		System.out.println("3단");
+//		System.out.println(3*1);
+//		System.out.println(3*2);
+//		System.out.println(3*3);
+//		System.out.println(3*4);
+//		System.out.println(3*5);
+//		System.out.println(3*6);
+//		System.out.println(3*7);
+//		System.out.println(3*8);
+//		System.out.println(3*9);
+	    
+		System.out.println("출력할 구구단 단은?");
+		Scanner scanner= new Scanner(System.in);
+		int number=scanner.nextInt();
+		System.out.println(number*1);
+		System.out.println(number*2);
+		System.out.println(number*3);
+		System.out.println(number*4);
+		System.out.println(number*5);
+		System.out.println(number*6);
+		System.out.println(number*7);
+		System.out.println(number*8);
+		System.out.println(number*9);
 		
 	}
 
