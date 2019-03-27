b0bdf397caa0dc700191e1af3df94d86a4d745c8
LIM <LIM@DESKTOP-1NABISU>
2019-03-27 13:33:39 +0200

    1

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,17 +1,27 @@
 
 class Gugudan{
 	public static int[] calculate(int times) {
-		int[] result=new int[10];	
-		for(int i=0;i<=9;i++) {
+		int[] result=new int[times];	
+		for(int i=0;i<result.length;i++) {
 			result[i]=times*(i+1);
 		}
 		return result;
 	}
 	
-	public static void print(int[] result){
-		for(int i=0;i<10;i++){
+	public static int[] calculate(int times, int b) {
+		
+		int[] result=new int[b];	
+		for(int i=0;i<=result.length;i++) {
+			result[i]=times*(i+1);
+		}public static void print(int[] result){
+		for(int i=0;i<result.length;i++){
 			System.out.println(result[i]);
 		}
 	}
+		return result;
 	}
+	
+	
+	
+}
 
--- src/GugudanMain.java
+++ src/GugudanMain.java
@@ -1,29 +1,30 @@
 import java.util.Scanner;
 
 public class GugudanMain {
-
+	
 	public static void main(String[] args) {
-
 		Scanner scanner=new Scanner(System.in);
-
 		String inputValue=scanner.nextLine();
-		
 		String[] splitedValue=inputValue.split(",");
 		
-		if(inputValue.contains(",")) {
+		if(inputValue.contains(",")) {	
+			int first  = Integer.parseInt(splitedValue[0]);
+			int second = Integer.parseInt(splitedValue[1]);
 			
-			int first = Integer.parseInt(splitedValue[0]);
-			int second= Integer.parseInt(splitedValue[1]);
+			int[] result=Gugudan.calculate(first);
+			Gugudan.print(result);	
+			
+			
 		}
 		else {
 			int first = Integer.parseInt(splitedValue[0]);
+			int[] result=Gugudan.calculate(first);
+			Gugudan.print(result);
+			}
 		}
 		
-			int[] result=Gugudan.calculate(first);
-			
-			
-		}	
-}
+	}	
+
 
 	
 	
